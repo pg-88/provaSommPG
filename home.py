2cc7f3aadecd69a21ab7d54aaf380e9272058958
Pietro Grigolo <grigolopi@gmail.com>
2023-05-03 12:59:30 +0200

    consegna

--- home.py
+++ home.py
@@ -26,6 +26,14 @@ def header():
 def authenticate(user, password) -> bool:
     '''non fa nulla oltre che controllare un json'''
     auth = False
+
+
+
+
+        
+
+
+        #st.write(df)
     with open("resource/users.json") as json_file:
         users = json.load(json_file)
         print(users)
@@ -58,8 +66,14 @@ def import_file_csv():
         #go_on = st.bu
         #st.write(df)
 
+def get_eq_str(intercept, betas, datafrtame):
+    eq_str = f"{round(intercept, 2)} "
+    for c in range(betas.size):
+        beta = round(betas[c],2)
+        col = datafrtame.columns[c]
+        eq_str += f"+ {beta} * {col}"
 
-
+    return eq_str
 
 def footer():
     st.write("page is ended... useful stuff usually goes here!")
@@ -87,47 +101,78 @@ def main() -> None:
             except:
                 st.write("something went wrong, check the link please!")
         
-        ##ripulisco il dataframe
-        df = df.iloc[0:-1] # elimino la rocket science
-        df = df.rename(columns={"medv": "price"}) # rinomino la colonna
-        df = df.astype(dtype="float64") # conversione dei tipi 
-        print(df.info())
+            ##ripulisco il dataframe
+            df = df.iloc[0:-1] # elimino la rocket science
+            df = df.rename(columns={"medv": "price"}) # rinomino la colonna
+            df = df.astype(dtype="float64") # conversione dei tipi 
+            print(df.info())
 
-        st.write(df)
+            st.write(df)
 
-        st.header("EDA:")
-        st.markdown("## Correlation Matrix")
+            st.header("EDA:")
+            st.markdown("## Correlation Matrix")
 
-        df.corr()
-        fig = plt.figure()
-        sns.heatmap(df.corr(), annot=True)
-        st.pyplot(fig)
+            df.corr()
+            fig = plt.figure()
+            sns.heatmap(df.corr(), annot=True)
+            st.pyplot(fig)
 
-        # fig1 = plt.figure()
-        # sns.pairplot(df, hue="price")
-        # st.pyplot(fig1)
-        
+            # fig1 = plt.figure()
+            # sns.pairplot(df, hue="price")
+            # st.pyplot(fig1)
+            
 
-        st.markdown("Crim against Price:")
-        fig2 = plt.figure()
-        sns.jointplot(x=df["crim"], y=df["price"])
-        st.pyplot(fig2)
+            st.markdown("Crim against Price:")
+            fig2 = plt.figure()
+            sns.scatterplot(x=df["crim"], y=df["price"])
+            st.pyplot(fig2)
 
-        st.markdown("## Creazione Modello: ")
+            st.markdown("## Creazione Modello: ")
+            X = df.drop(columns="price") # feature
+            y = df["price"] # target
+            
+            X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.33)
 
-        X = df.drop(columns="price") # feature
-        y = df["price"] # target
-        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.33)
+            price_model = LinearRegression(fit_intercept=True)
+            price_model.fit(X_train, y_train)
+            coeff = price_model.coef_
+            intercept = price_model.intercept_
 
-        
+            st.write(get_eq_str(intercept,coeff,df))
 
+            joblib.dump(price_model, "price.pkl")
+            st.write("model at: price.pkl")
 
-        
+            y_pred = price_model.predict(X_test)
+            y_arr = y_test.to_numpy()
+            test = pd.DataFrame(y_test.to_numpy(), columns=["Test Set"])
+            pred = pd.DataFrame(y_pred, columns=["Prediction Set"])
 
+            fig3 = plt.figure()
+            sns.lineplot(y_pred)
+            sns.lineplot(y_arr)
+            st.pyplot(fig3)
 
-        #st.write(df)
+            st.metric("Model Errors R²", round(r2_score(y_test, y_pred),4))
+            st.metric("Mean Absolute Error", round(mean_absolute_error(y_test, y_pred), 3))
+            st.metric("Mean Squared Error", round(mean_squared_error(y_test, y_pred),3))
+            st.metric("Root Mean Square Root", round(mean_squared_error(y_test, y_pred, squared=True),3))
 
+            model = joblib.load("price.pkl")
+            
+            inserted = []
+            cols = df.columns
+            st.write(cols)
 
+            for c in range(cols.size):
+                inserted.append(st.number_input(f"Value for {cols[c]}"))
+                
+
+            ins = pd.DataFrame(inserted)
+
+            st.write(ins)
+            
+            model.predict(ins)
     
 
 
